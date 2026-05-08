faaa19852c2817aec131bf3a8faa518ced284fa6
MaySVT <20307130318@fudan.edu>
2026-05-07 20:16:42 -0400

    Merge remote-tracking branch 'upstream/main' into feature/harvest_moon

--- quick_tests.sh
+++ quick_tests.sh
@@ -2,7 +2,6 @@
 
 variants=("pokemon_red" "pokemon_crystal" "pokemon_brown" "pokemon_prism" "pokemon_fools_gold" "pokemon_starbeasts" "sword_of_hope_1" "sword_of_hope_2" "deja_vu_1" "deja_vu_2" "harvest_moon_1" "harvest_moon_2" "harvest_moon_3" "harry_potter_philosophers_stone" "legend_of_zelda_links_awakening" "legend_of_zelda_the_oracle_of_seasons")
 
-
 echo "Testing emulator..."
 for variant in "${variants[@]}"; do
     echo "  Variant: $variant"
